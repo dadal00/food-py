e40440c249bc449963d5303d2638ae19c86b5678
dadal00 <dylanadal.00@gmail.com>
2025-11-13 11:29:58 -0500

    base food + ci

---
+++ constants.py
@@ -0,0 +1,31 @@
+URL = "https://api.hfs.purdue.edu/menus/v3/GraphQL"
+
+PAYLOAD = {
+    "operationName": "getLocationMenu",
+    "variables": {"date": "2025-11-13"},
+    "query": """
+    query getFoodNames($date: Date!) {
+      diningCourts {
+        formalName\n
+        dailyMenu(date: $date) {
+          meals {
+            name
+            stations {
+              name
+              items {
+                item {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    """,
+}
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0",
+}
---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from constants import URL, HEADERS, PAYLOAD
+import re
+
+response = requests.post(URL, headers=HEADERS, json=PAYLOAD)
+
+if not response.ok:
+    print("Error: ", response.status_code, response.text)
+    exit()
+
+text = response.text
+
+splitByCourt = re.split(r"(?=\"formalName\":)", text)
+
+diningCourts = {}
+
+for court in splitByCourt[1:]:
+    diningCourts[re.findall(r'"formalName":\s*"([^"]+)"', court)[0]] = set(
+        re.findall(r'"item":\s*{\s*"name":\s*"([^"]+)"', court)
+    )
+
+print(diningCourts)
