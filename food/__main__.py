3604202b3f570976fd86bb9798e0c979bc966a58
dadal00 <dylanadal.00@gmail.com>
2025-11-14 16:43:16 -0500

    ci fix

--- food/__main__.py
+++ food/__main__.py
@@ -17,12 +17,10 @@ if __name__ == '__main__':
     menu = set()
 
     for court in splitByCourt[1:]:
-        courtMenu = set(
-            re.findall(r'"item":\s*{\s*"name":\s*"([^"]+)"', court)
-        )
-        
+        courtMenu = set(re.findall(r'"item":\s*{\s*"name":\s*"([^"]+)"', court))
+
         diningCourts[re.findall(r'"formalName":\s*"([^"]+)"', court)[0]] = courtMenu
-        
+
         menu = menu.union(courtMenu)
 
     print(len(menu))
